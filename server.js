eaed5ba70c5565dc5a849047564f7497a78338de
Chris Kalafarski <chris@farski.com>
2016-04-18 21:56:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -704,7 +704,7 @@ function displayLeaderboard(message, game, playerId)
 {
   // do single score based on params
   var text = 'Current Scoreboard:\n';
-  text = text + 'player |  1  |  2  |  3  |  4  |  5  |  6  |  UB  ||  3ok  |  4ok  |  dt  |  ss  |  ls  |  ??  |  y!  |  yb!  |  turn  | total\n';
+  text = text + 'player |  1  |  2  |  3  |  4  |  5  |  6  ||  UB  ||  3ok  |  4ok  |  dt  |  ss  |  ls  |  ??  |  y!  |  yb!  |  turn  | total\n';
   
   if(playerId && playerId != null && playerId != 'undefined'){
     Score.find({ channelId: game.channelId, userId: playerId }, function (err, scores) {
@@ -713,7 +713,7 @@ function displayLeaderboard(message, game, playerId)
       if(scores.length > 0){
         for(score in scores){
           var line = scores[score].userId + ' |' + displayScore(scores[score].ones) + '|' + displayScore(scores[score].twos) + '|' + displayScore(scores[score].threes)
-            + '|' + displayScore(scores[score].fours) + '|' + displayScore(scores[score].fives) + '|' + displayScore(scores[score].sixes) + '|' + displayScore(scores[score].upperBonus) + 
+            + '|' + displayScore(scores[score].fours) + '|' + displayScore(scores[score].fives) + '|' + displayScore(scores[score].sixes) + '||' + displayScore(scores[score].upperBonus) + 
             ' ||' + displayScore(scores[score].threeOK) + '|' + displayScore(scores[score].fourOK) + '|' + displayScore(scores[score].fullHouse) + '|' + displayScore(scores[score].smallStraight) +
              '|' + displayScore(scores[score].largeStraight) + '|' + displayScore(scores[score].chance) + '|' + displayScore(scores[score].yahtzee) + '|'
              + displayScore(scores[score].yahtzeeBonus) + '|' + displayScore(scores[score].turnCount) + '|' + scores[score].total() + '\n';
